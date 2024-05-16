72f38a842fc29c7173c27fca5ee3db0162580225
GiacomoLoat <giacomo.loat@studenti.unipd.it>
2024-05-16 18:56:16 +0200

    Test3
    close #4

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,17 @@
 // [Giacomo] [Loat] [2077677] 
 // [Riccardo] [Pontello] [2076441] 
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+
+}
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,4 +2,17 @@
 // [Giacomo] [Loat] [2077677] 
 // [Riccardo] [Pontello] [2076441] 
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanPrinterTest {
+    @Test
+    public void testPrint() {
+        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
+        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
+        assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
+    }   
+
+}
